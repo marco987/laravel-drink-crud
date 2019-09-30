052352b3f88408cfe9cca75bfca5b21efba38691
marco987 <pacemarco87@gmail.com>
2019-09-30 15:56:44 +0200

    Controller

--- app/Http/Controllers/MyController.php
+++ app/Http/Controllers/MyController.php
@@ -3,8 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Drink;
 
 class MyController extends Controller
 {
-    //
+  public function getDrink() {
+
+    $drinks = Drink::all();
+
+    $prezzoMax = $drinks->max('prezzo');
+    $prezzoMin = $drinks->min('prezzo');
+    $prezzoAvg = $drinks->avg('prezzo');
+
+    return view('drink', compact('drinks', 'prezzoMax', 'prezzoMin', 'prezzoAvg'));
+  }
 }
